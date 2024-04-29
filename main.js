b0d326702afa5b6afa9dc759aab8e841740915bd
Gabriel Marini <gabrielmarini12@icloud.com>
2024-04-29 19:33:39 -0300

    Associado a ação ao evento de submit

---
+++ main.js
@@ -0,0 +1,33 @@
+import { generateReturnsArray } from "./src/investmentGoals";
+
+const form = document.getElementById("investment-form");
+
+function renderProgression(evt) {
+  evt.preventDefault();
+  const startingAmount = Number(
+    document.getElementById("starting-amount").value
+  );
+
+  const additionalContribuition = Number(
+    document.getElementById("additional-contribuition").value
+  );
+
+  const timeAmount = Number(document.getElementById("time-amount").value);
+
+  const timeAmountPeriod = document.getElementById("time-amount-period").value;
+  const returnRate = Number(document.getElementById("return-rate").value);
+
+  const returnRatePeriod = document.getElementById("evaluation-period").value;
+  const taxRate = Number(document.getElementById("tax-rate").value);
+
+  const returnsArray = generateReturnsArray(
+    startingAmount,
+    timeAmount,
+    timeAmountPeriod,
+    additionalContribuition,
+    returnRate,
+    returnRatePeriod
+  );
+}
+
+form.addEventListener("submit", renderProgression);
